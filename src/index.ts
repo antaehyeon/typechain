30f00ac849f4c2b3634f4e2dcc63c655747547c0
HYEON(Mac) <antaehyeon@live.co.kr>
2019-02-28 00:52:13 +0900

    Feat: Classes on Typescript 1

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,15 @@
-interface Human {
-  name: string;
-  age: number;
-  gender: string;
+class Human {
+  public name: string;
+  public age: number;
+  public gender: string;
+  constructor(name: string, age: number, gender: string) {
+    this.name = name;
+    this.age = age;
+    this.gender = gender;
+  }
 }
 
-const person = {
-  name: "hyeon",
-  age: 22,
-  gender: "male"
-};
+const hyeon = new Human("HYEON", 26, "male");
 
 const sayHi = (person: Human): string => {
   return `Hello ${person.name}, you are ${person.age}, you are a ${
@@ -16,6 +17,6 @@ const sayHi = (person: Human): string => {
   }`;
 };
 
-console.log(sayHi(person));
+console.log(sayHi(hyeon));
 
 export {};
